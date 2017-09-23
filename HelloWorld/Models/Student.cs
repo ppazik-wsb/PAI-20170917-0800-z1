7097c1fd5fe8862945bf9b493cb4b02f48a5c5a1
ppaz <przemyslaw.pazik@gmail.com>
2017-09-24 00:30:07 +0200

    Settery i gettery properties

--- HelloWorld/Models/Student.cs
+++ HelloWorld/Models/Student.cs
@@ -11,5 +11,20 @@ namespace HelloWorld.Models
         public int ID { get; set; }
         public string Imie { get; set; }
         public string Nazwisko { get; set; }
+
+        private DateTime dataNarodzin;
+
+        public DateTime DataNarodzin
+        {
+            get { return dataNarodzin; }
+            set {
+                if (value < DateTime.Parse("01-01-1900 00:00:00") || value > DateTime.Now)
+                {
+                    throw new ApplicationException("Data narodzin studenta poza dozwolonym zakresem!");
+                }
+
+                dataNarodzin = value;
+            }
+        }
     }
 }
