a75d229d3f1ecfae36e2be856568ad6f58cbcfc2
ppaz <przemyslaw.pazik@live.com>
2017-10-16 08:06:26 +0200

    Clean MVC project with user accounts

---
+++ PAI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PAI.Web.Startup))]
+namespace PAI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
