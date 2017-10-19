d8e98f4150c2319f16b608b9bcc919b80d397803
ppaz <przemyslaw.pazik@live.com>
2017-10-19 07:36:03 +0200

    Simple selfhosting WebApi app using Owin and Katana.

---
+++ PAI.Selfhosted.WebApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAI.Selfhosted.WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server started on {url}");
+                Console.ReadKey();
+
+                Console.WriteLine("Server is stopping...");
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
