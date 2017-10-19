c05fdef69fac1c22fd9e489a0baaae9a1beab183
ppaz <przemyslaw.pazik@live.com>
2017-10-19 10:39:19 +0200

    OWIN lightweight WebApi Endpoint implementation.

--- PAI.Selfhosted.WebApi/Program.cs
+++ PAI.Selfhosted.WebApi/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.Owin;
+using System.Web.Http;
 
 namespace PAI.Selfhosted.WebApi
 {
@@ -14,12 +15,13 @@ namespace PAI.Selfhosted.WebApi
     {
         static void Main(string[] args)
         {
-            string url = "http://localhost:8080";
+            string url = "http://localhost:8081";
 
             using (WebApp.Start<Startup>(url))
             {
                 Console.WriteLine($"Server started on {url}");
-                Console.ReadKey();
+                Console.WriteLine("Press Enter to stop server.");
+                Console.ReadLine();
 
                 Console.WriteLine("Server is stopping...");
             }
@@ -30,14 +32,38 @@ namespace PAI.Selfhosted.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            app.Use(async (envirotment, next) =>
+            {
+                Console.WriteLine("Request: " + envirotment.Request.Path);
+
+                await next();
+
+                Console.WriteLine("Response: " + envirotment.Response.StatusCode);
+            });
+
+            ConfigureWebApi(app);
+
             // Gdy jest podpięty ten kontroler do pipeline, będziemy dostawać wypis wszystkich wpisów do środowiska oraz dopisze swój nowy wpis WebApiFive.
             app.Use<HighFiveComponent>();
 
             // Komponent LowFive będzie sprawdzał czy dany klucz występuje w środowisku i wypisze jego wartość. Na koniec wyświetli zawartość odpowiedzi do klienta.
             app.Use<LowFiveComponent>();
 
-            // By przetestować welcome page skompiluj, uruchom aplikację i w swojej przeglądarce np. Chrome wpisz http://localhost:8080/
+            // By przetestować welcome page skompiluj, uruchom aplikację i w swojej przeglądarce np. Chrome wpisz http://localhost:8081/
             app.UseWelcomePage();
+
+
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html")); // Obsługa serializacji do JSON zamiast standardowo XML
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new
+            {
+                id = RouteParameter.Optional
+            });
+            app.UseWebApi(config); // Dorzucenie obsługi WebApi do OWIN
         }
     }
 
---
+++ PAI.Selfhosted.WebApi/SandwichController.cs
@@ -0,0 +1,34 @@
+﻿using PAI.Selfhosted.WebApi.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PAI.Selfhosted.WebApi
+{
+    public class SandwichController : ApiController
+    {
+        static Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+        [HttpGet]
+        public ICollection<Sandwich> Get()
+        {
+            List<Sandwich> result = new List<Sandwich>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new Sandwich { Id = i, Name = "Mexico Specialite #" + i, Price = Math.Round(rnd.NextDouble() * 5, 2) + 5 });
+            }
+
+            return result;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(new Sandwich { Id = 123, Name = "Mexico Specialite", Price = Math.Round(rnd.NextDouble() * 5, 2) + 5 });
+        }
+    }
+}
