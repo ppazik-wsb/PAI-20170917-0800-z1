19c9ab72c1f99540897a88dde9cfd8b3163b5107
ppaz <przemyslaw.pazik@live.com>
2017-10-19 08:46:57 +0200

    Custom Owin Middleware components in the processing pipeline.

--- PAI.Selfhosted.WebApi/Program.cs
+++ PAI.Selfhosted.WebApi/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Owin;
+
 namespace PAI.Selfhosted.WebApi
 {
     class Program
@@ -28,7 +30,61 @@ namespace PAI.Selfhosted.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            // Gdy jest podpięty ten kontroler do pipeline, będziemy dostawać wypis wszystkich wpisów do środowiska oraz dopisze swój nowy wpis WebApiFive.
+            app.Use<HighFiveComponent>();
+
+            // Komponent LowFive będzie sprawdzał czy dany klucz występuje w środowisku i wypisze jego wartość. Na koniec wyświetli zawartość odpowiedzi do klienta.
+            app.Use<LowFiveComponent>();
+
+            // By przetestować welcome page skompiluj, uruchom aplikację i w swojej przeglądarce np. Chrome wpisz http://localhost:8080/
             app.UseWelcomePage();
+        }
+    }
+
+    // Middleware component - dodatkowy komponent w pipeline Owin/Katana
+    public class HighFiveComponent
+    {
+        Func<IDictionary<string, object>, Task> _next;
+
+        public HighFiveComponent(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> enviroment)
+        {
+            var envCopy = new Dictionary<string, object>(enviroment);
+
+            foreach (var item in enviroment)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+            enviroment.Add("WebApiFive", "To jest mój nowy klucz :)");
+
+            await _next(enviroment);
+        }
+    }
+
+    public class LowFiveComponent
+    {
+        Func<IDictionary<string, object>, Task> _next;
+
+        public LowFiveComponent(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> enviroment)
+        {
+            IOwinContext ctx = new OwinContext(enviroment);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(enviroment.ContainsKey("WebApiFive") ? $"Wartość klucza WebApiFive: {enviroment["WebApiFive"]}" : "Brakuje klucza WebApiFive!!!");
+            Console.ResetColor();
+
+            await _next(enviroment);
         }
     }
 }
